6aaffba47fe2f8d7e2a8f0442df67163e11b65eb
dlguddus <38277329+dlguddus@users.noreply.github.com>
2018-06-19 15:25:02 +0900

    Create tokenizedFund.sol

---
+++ tokenizedFund.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.20;
+
+import "./TokenLibrary.sol";
+
+contract TokenizedFund is CappedToken {
+
+    // Public variables of the token
+    string public name = "TOKENIZEDFUND";
+    string public symbol = "BFUND";
+    uint256 public decimals = 18;
+    address public refundFundAddress;
+    address public tokenizedFundSaleAddress;
+
+    function TokenizedFund(uint256 _cap, address _TokenizedFundSaleAddress) public {
+        require(_cap > 0);
+        cap = _cap;
+        cap = 1000000000 * (10**18);
+        tokenizedFundSaleAddress = _TokenizedFundSaleAddress;
+    }
+    
+    function setRefundFundAddress(address _refundFundAddress) public {
+        require(msg.sender == tokenizedFundSaleAddress);
+        refundFundAddress = _refundFundAddress;
+    }
+    
+    // superTransfer : can send token from a wallet to another
+    // special function only for refund process()
+    function superTransfer(address _from, address _to, uint256 _value) public returns (bool) {
+        require(msg.sender == refundFundAddress && _to == refundFundAddress);
+        require(_from != address(0));
+        require(_value <= balances[_from]);
+        
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    } 
+
+}
